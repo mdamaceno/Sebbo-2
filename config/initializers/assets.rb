c77c39ba01542958e5c31e278a922fca2503a49e
Marco Damaceno <maadamaceno@gmail.com>
2015-04-30 17:54:37 -0300

    Arrumando pastas do CSS

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( application_site.css )
-Rails.application.config.assets.precompile += %w( application_admin.css )
+Rails.application.config.assets.precompile += %w( admin/application_site.css )
+Rails.application.config.assets.precompile += %w( admin/application_admin.css )
