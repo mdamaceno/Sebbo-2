b8fd8ec5b6b07c00ac4a03dc2817d5a6259c01c7
Marco Damaceno <maadamaceno@gmail.com>
2015-05-01 13:58:09 -0300

    Ajustes no Bootstrap

--- app/controllers/site/products_controller.rb
+++ app/controllers/site/products_controller.rb
@@ -12,30 +12,6 @@ class ProductsController < ApplicationController
     respond_with(@product)
   end
 
-  def new
-    @product = Product.new
-    respond_with(@product)
-  end
-
-  def edit
-  end
-
-  def create
-    @product = Product.new(product_params)
-    @product.save
-    respond_with(@product)
-  end
-
-  def update
-    @product.update(product_params)
-    respond_with(@product)
-  end
-
-  def destroy
-    @product.destroy
-    respond_with(@product)
-  end
-
   private
     def set_product
       @product = Product.find(params[:id])
