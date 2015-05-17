e9f58a3c3b494acd00d78874a5edce21861b7164
Marco Damaceno <maadamaceno@gmail.com>
2015-05-17 20:16:21 -0300

    Rake assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( fonts images )
+Rails.application.config.assets.precompile += %w( other )
 
 Rails.application.config.assets.precompile += %w( site/application_site.css )
 Rails.application.config.assets.precompile += %w( admin/application_admin.css )
